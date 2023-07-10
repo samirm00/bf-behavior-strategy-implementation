c2ff86875deca274a49961ebe3d7afead4fdc384
samirm00 <sammoham.be@gmail.com>
2023-07-10 08:06:37 +0200

    initial commit

--- 2-write/4-test-driven-development/exercises/count-characters.js
+++ 2-write/4-test-driven-development/exercises/count-characters.js
@@ -1,16 +1,11 @@
+'use strict';
+
 /**
  * Counts how many times each character appears in a string.
  *
  * @param {string} [text=''] - The string of characters to count.
  * @returns {number{}} An object of key/value pairs counting each character.
- *
- * @example
- *
- * countCharacters('hi'); // { h: 1, i: 1 }
- *
- * @example
- *
- * countCharacters('hiiii'); // { h: 1, i: 4 }
- *
+ * @example countCharacters('hi'); // { h: 1, i: 1 }
+ * @example countCharacters('hiiii'); // { h: 1, i: 4 }
  */
 export const countCharacters = (text = '') => {};
